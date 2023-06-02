69fd3f05a4a78bd03f31394b94a96713315135ab
Jelle Fiere <99071834@mydavinci.nl>
2023-06-02 13:43:51 +0200

    .

---
+++ opdrachten/back-end-2/welkom/welkom.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>De ingevulde gegevens zijn: </h1>
+<?php 
+echo "Your name: ", $_POST["name"], "<br><br>";
+echo "Your email: ", $_POST["email"];
+?>
+
+    
+</body>
+</html>
+
+
