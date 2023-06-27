1e33ab632733d42aac480f5560d9b060afa290d4
Jelle Fiere <99071834@mydavinci.nl>
2023-06-27 11:25:42 +0200

    Update onkunde.php

--- opdrachten/back-end-2/mad libs/onkunde.php
+++ opdrachten/back-end-2/mad libs/onkunde.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
     <title>onkunde antwoord</title>
 </head>
 <body>
-    
+    <div id="main">
+        <img src="img/madlibs.png" alt="madlibs logo" height="120" width="400">
+        <div class="header">
+            <button>er heerst paniek</button>
+            <button>onkunde</button>
+        </div>
+        <div class="main">
+            <?php ?>
+        </div>
+        <div class="footer">
+            <p>Deze website is gemaakt door Pingle</p>
+        </div>
+    </div>
 </body>
 </html>
